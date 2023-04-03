003a86e4187b965fc0dc0bd72862c99b92464635
EmrullahUcel <emrullahucel@gmail.com>
2023-04-04 00:06:24 +0300

    commit

--- src/map.jsx
+++ src/map.jsx
@@ -3,14 +3,15 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function LocationMarker() {
   const [markers] = useState([
-    [51.505, -0.09],
-    [51.505, -0.02],
-    [51.505, -0.01],
+    [41.043615052454435, 28.875784211220004],
+    [41.04375217749469, 28.876010680065452],
+    [41.04346349286224, 28.87547640499433]
+    ,
   ]);
   return (
     <MapContainer
       style={{ width: "100vw", height: "100vh" }}
-      center={[51.505, -0.09]}
+      center={[41.04362838398769, 28.875784857246703]}
       zoom={12}
       scrollWheelZoom={false}
     >
@@ -21,7 +22,7 @@ function LocationMarker() {
       {markers.map((m, i) => (
         <Marker key={i} position={m}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            Çöp kovası
           </Popup>
         </Marker>
       ))}
